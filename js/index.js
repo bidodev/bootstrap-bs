acb6e09f4a3f674a18591710d8c41fb3a29e133a
Claudinei Bido <bido.c@live.com>
2020-03-30 15:17:42 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,6 @@
+// import another component
+import main from "./main";
+main();
+
+let rat = 5;
+console.log(`${rat}`);
---
+++ js/main.js
@@ -0,0 +1,6 @@
+// import a CSS module
+import classes from "../sass/main.scss";
+
+export default () => {
+  console.log(classes.main);
+};
